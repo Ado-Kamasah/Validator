157d1544c6e6f5ff19a2c9fb60d716c71ea4afaf
amosikure <amosikure7@gmail.com>
2025-02-11 15:54:39 +0000

    animation

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,26 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-     <main class="bg-gray-300 h-screen flex justify-center items-center">
-  <RouterView/>
-  </main>
+  <main class="bg-gray-300 h-screen flex justify-center items-center scroll-smooth">
+    <transition name="scroll" mode="out-in">
+    <RouterView/>
+    </transition>
+</main>
 </template>
-
+<style>
+.scroll-enter-active,
+.scroll-leave-active{
+  transition: transform 3s ease, opacity 3s ease;
+}
+.scroll-enter-from{
+  opacity: 0;
+  transform: translate(20px);
+}
+.scroll-leave-to{
+  opacity: 0;
+  transform: translate(-20px);
+}
+.scroll-smooth{
+  scroll-behavior: smooth;
+}
+</style>
