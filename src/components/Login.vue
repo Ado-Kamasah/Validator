c4f7c3a5a8137de3d3718598489936e8b8d73d4e
Maderobills <dennisbortey60@gmail.com>
2025-02-11 13:48:40 +0000

    login done

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,10 @@
 <template>
-    <h1>Login Here</h1>
+    
+    <div class="bg-white p-6 w-86 rounded-lg shadow-2xl flex flex-col justify-center gap-5">
+        <h1 class="text-gray-600 font-medium">Login Here</h1>
+       <form action="" method="post" class="w-full flex flex-col gap-2.5">
+        <input type="text" class="px-4 py-2 text-gray-800 border border-gray-100 rounded-md bg-gray-300" placeholder="Enter your index number">
+        <button class="p-2 px-4 bg-gray-700 rounded-md text-white font-medium">Validate</button>
+       </form>
+    </div>
 </template>
