8ac126fb3b77913b25fed6d6b1766a476000ddc4
amosikure <amosikure7@gmail.com>
2025-02-11 14:13:59 +0000

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,6 +6,7 @@
         <div class="mt-3">
             <label for="indexnumber" class="block text-base mb-2">Index number</label>
             <input type="text" placeholder="Enter index number" class="border w-full text-base px-2 py-1 focus:outline-non focus:ring-0 focus:border-gray-600 bg-gray-600">
+            
         </div>
     </div>
 </template>
