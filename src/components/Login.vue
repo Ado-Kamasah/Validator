a802f81e8f8a4c413775d21a5edc3e5eeb67a593
Ado-Kamasah <161648390+Ado-Kamasah@users.noreply.github.com>
2025-02-11 14:06:59 +0000

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>Login Here</h1>
+    <div class="flex flex-col bg-gray-300 pb-2 shadow-white items-center min-h-screen">
+    <h1 class="text-2xl pb-9  text-white-700">Login Here</h1>
+    <div class="rounded-md text-center shadow-gray-500">
+        <p class="text-white-500 mt-2">Give me your details </p> 
+        <input type="text" placeholder="username" class="w-full mt-2 p-2 border rounded-lg">
+         <input type="Password" placeholder="password" class="w-full mt-2 p-2 border rounded-lg">
+         <button class="w-full mt-4 bg-lime-600 text-white py-2 rounded-lg">login</button>
+         
+    </div>
+</div>
 </template>
