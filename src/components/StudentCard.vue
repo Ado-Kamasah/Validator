324badca259ac7f56032a4fde3cb078b1e86fccd
Ado-Kamasah <161648390+Ado-Kamasah@users.noreply.github.com>
2025-02-18 14:19:53 +0000

    day2

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="bg-white flex flex-col justify-center shadow-sm rounded-md ">
-        <img src="https://img.freepik.com/free-photo/image-adult-african-american-man-holding-notebooks-smiling-studying-courses-standing-pink-background_1258-76952.jpg?t=st=1739879237~exp=1739882837~hmac=6f5d7abefe076b1be9fc193054aa4b6583001811c1c40a47a36d7b25dde74cf2&w=740" class="w-full max-w-80"  alt="">
+        <img :src="studentimage" class="w-full max-w-80"  alt="">
         
        <div class="p-2">
-        <h1 class="text-sm ">Name: Adjei Adjei</h1>
+        <h1 class="text-sm ">{{ name }}</h1>
         <div>
-            <p>Index DCM0012</p>
-            <span>level 200</span>
+            <p>{{ IndexNo }}</p>
+            <span>{{ Level }}</span>
         </div>
         <h4>Information Communication </h4>
     </div>
 </div>
 </template>
+<script setup>
+defineProps(['name','IndexNo','Level','studentimage'])
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <script setup>
 import StudentCard from '@/components/StudentCard.vue';
-
+import studentlist from '@/store/studentlist'
 </script>
 
 <template>
       <main class="bg-gray-300 h-screen flex justify-center items-center">
-<StudentCard/>
+<div class="grid grid-cols-5 gap-4">
+<StudentCard v-for="student in studentlist" 
+:name="student.name"
+:IndexNo="student.IndexNo"
+:Level="student.Level"
+:studentimage="student.studentimage"/>
+
+</div>
 </main>
 </template>
