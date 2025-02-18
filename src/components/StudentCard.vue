28545e6b59630d41083c311de402ba5e68182f5e
Ado-Kamasah <161648390+Ado-Kamasah@users.noreply.github.com>
2025-02-18 12:31:07 +0000

    CRD

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
-        <img src="" alt="">
-        <h1>Student Name Here</h1>
+    <div class="bg-white flex flex-col justify-center shadow-sm rounded-md ">
+        <img src="https://img.freepik.com/free-photo/image-adult-african-american-man-holding-notebooks-smiling-studying-courses-standing-pink-background_1258-76952.jpg?t=st=1739879237~exp=1739882837~hmac=6f5d7abefe076b1be9fc193054aa4b6583001811c1c40a47a36d7b25dde74cf2&w=740" class="w-full max-w-80"  alt="">
+        
+       <div class="p-2">
+        <h1 class="text-sm ">Name: Adjei Adjei</h1>
         <div>
-            <p>indexnumber</p>
-            <span>level 000</span>
+            <p>Index DCM0012</p>
+            <span>level 200</span>
         </div>
-        <h4>course read</h4>
+        <h4>Information Communication </h4>
     </div>
+</div>
 </template>
