6ac6de54455dbaaecd938fa7520eea736a062c3a
Victoria <toribamdel@gmail.com>
2025-02-25 13:06:26 +0100

    i changed nothing hehehehe

--- src/components/Ui/Typography.vue
+++ src/components/Ui/Typography.vue
@@ -1,11 +1,12 @@
 <script setup>
-    defineProps(['content'])
+    defineProps(['content', 'textcolor'])
 </script>
 <template>
+   <div v-if="content">
     <h1 v-if="content.h1"class="text-8xl font-bold text-orange-600 m-auto">
         {{ content.h1 }}
     </h1>
-    <h2 v-if="content.h2"class="text-6xl font-bold text-blue-600 m-auto">
+    <h2 v-if="content.h2"class="text-6xl font-bold m-auto" :class=" textcolor">
         {{ content.h2 }}
     </h2>
     <h3 v-if="content.h3"class="text-4xl font-bold text-green-600 m-auto">
@@ -14,4 +15,6 @@
     <h4 v-if="content.h4"class="text-2xl font-bold text-amber-600 m-auto">
         {{ content.h4 }}
     </h4>
+    </div>
+    <h5 v-else> No Typography</h5>    
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,20 @@
 import StudentCard from '@/components/StudentCard.vue';
 import studentlist from '@/store/studentlist'
 import Typography from '@/components/Ui/Typography.vue'; 
+
+const headings=([
+      {h1:'Attractive people'} , {h2:'People'}
+])
 </script>
 
 <template>
       <main class="bg-gray-300 h-screen flex flex-col justify-center items-center">
-            <Typography :content="{h1:'Attractive people'}"/>
-            <Typography :content="{h2:'Attractive people'}"/>
+            <Typography v-for="heading in headings" :content="heading"/>
+            <Typography :content="{h2:'Attractive people'}" textcolor="text-green-300"/>
             <Typography :content="{h3:'Attractive people'}"/>
             <Typography :content="{h4:'Attractive people'}"/>
 <div class="grid grid-cols-5 gap-4">
-      
+
 <StudentCard v-for="student in studentlist" 
 :name="student.name"
 :IndexNo="student.IndexNo"
